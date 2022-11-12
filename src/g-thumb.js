9c3db9806af86082145c97c3c74aa6b141fefdef
mrbrahman <shreyasls@yahoo.com>
2022-11-11 23:53:44 -0500

    Made helper methods private. Also, not using "set"ters where a helper method needs to set a value

--- src/g-thumb.js
+++ src/g-thumb.js
@@ -1,8 +1,8 @@
 class GThumb extends HTMLElement {
   // instance variables
-  _photoid; _width; _height; _rating; _selected;
+  #photoid; #width; #height; #rating; #selected;
   
-  dppx = parseFloat(window.devicePixelRatio.toFixed(2));
+  #dppx = parseFloat(window.devicePixelRatio.toFixed(2));
   
   static get observedAttributes() {
     return ['photoid','rating','width','height','selected'];
@@ -11,14 +11,14 @@ class GThumb extends HTMLElement {
   constructor() {
     // console.log('in constructor')
     super().attachShadow({mode: 'open'}); // sets "this" and "this.shadowRoot"
-    // console.log(this.dppx)
+    // console.log(this.#dppx)
   }
 
   connectedCallback() {
     
     // console.log('in connected callback');
     // TODO: handle this properly. rating can have 0 value
-    // if(!(this._photoid && this._rating && this._width && this._height) ){
+    // if(!(this.#photoid && this.#rating && this.#width && this.#height) ){
     //   return;
     // }
     
@@ -28,18 +28,20 @@ class GThumb extends HTMLElement {
 
     
     // create a placeholder regardless of whether the element is still in DOM
-    this.paintWidth();
-    this.paintHeight();
+    this.#paintWidth();
+    this.#paintHeight();
     // console.log(this.parentNode.offsetWidth);
     
     // wait for an arbitrary 250ms and create & paint the rest of the shadow DOM
     // this is so that in case the user is scrolling too fast, we don't download the image unnessarily or call the other sl (shoelace) web components or setup the listeners
-    setTimeout(this.paintRest(), 250);
+    setTimeout(this.#paintRest(), 250);
     
   }
   
   attributeChangedCallback(name, oldValue, newValue) {
     // console.log(`setting ${name} from *${oldValue}* to *${newValue}*`)
+
+    // use the "setters" to set the new values, so that any logic can be done in one place
     switch(name){
       case 'photoid':
         this.photoid = newValue;
@@ -65,12 +67,12 @@ class GThumb extends HTMLElement {
     // shadow DOM anyway, and the listeners should get automatically removed by browser
     
     // console.log('in dicsonnectedCallback');
-    this.shadowRoot.querySelector('input[type="checkbox"]').removeEventListener('click', this.handleSelection);
-    this.shadowRoot.querySelector('sl-rating').removeEventListener('deleted', this.slRatingChanged);
-    this.shadowRoot.querySelector('sl-icon-button[name="trash"]').removeEventListener('sl-change', this.itemDeleted);
+    this.shadowRoot.querySelector('input[type="checkbox"]').removeEventListener('click', this.#handleSelection);
+    this.shadowRoot.querySelector('sl-rating').removeEventListener('deleted', this.#slRatingChanged);
+    this.shadowRoot.querySelector('sl-icon-button[name="trash"]').removeEventListener('sl-change', this.#itemDeleted);
   }
   
-  paintRest(){
+  #paintRest(){
     // if the user is scrolling too fast, and the element is already removed, do not paint anything further
     if(!this.isConnected){
       return;
@@ -87,32 +89,33 @@ class GThumb extends HTMLElement {
     `
     
     // now paint them
-    this.paintSrc();
-    this.paintRating();
-    this.paintSelected();
+    this.#paintSrc();
+    this.#paintRating();
+    this.#paintSelected();
     
     // setup event listeners
     this.shadowRoot.querySelector('input[type="checkbox"]')
-      .addEventListener('click', this.handleSelection.bind(this));
+      .addEventListener('click', this.#handleSelection.bind(this));
     
     this.shadowRoot.querySelector('sl-rating')
-      .addEventListener('sl-change', this.slRatingChanged.bind(this));
+      .addEventListener('sl-change', this.#slRatingChanged.bind(this));
     
     this.shadowRoot.querySelector('sl-icon-button[name="trash"]')
-    .addEventListener('click', this.itemDeleted.bind(this))
+    .addEventListener('click', this.#itemDeleted.bind(this))
     
   }
 
-  handleSelection(evt){
-    this.selected = evt.target.checked;
+  #handleSelection(evt){
+    this.#selected = evt.target.checked;
   }
 
-  slRatingChanged(evt){
-    console.log(`new value: ${evt.target.value} to be updated in db`);
-    this.rating = evt.target.value;
+  #slRatingChanged(evt){
+    let r = evt.target.value
+    console.log(`new value: ${r} to be updated in db`);
+    this.#rating = r;
 }
   
-  itemDeleted(evt){
+  #itemDeleted(evt){
     // TODO: delete item from system (make REST call)
     // console.log('dispatching delete event');
     console.log('Delete from server/db here for photoid '+this.photoid);
@@ -129,7 +132,7 @@ class GThumb extends HTMLElement {
   // individual paint functions
   // checking for this.isConnected (i.e in DOM) in each, as these also get triggered for static elements
   // that use attributeChangedCallback to set the values before connectedComponents is called
-  paintWidth(){
+  #paintWidth(){
     if(this.isConnected){
       this.shadowRoot.getElementById('container').style.width = this.width+'px';
       // img element is not present during initial paint
@@ -138,7 +141,7 @@ class GThumb extends HTMLElement {
       }
     }
   }
-  paintHeight(){
+  #paintHeight(){
     if(this.isConnected){
       this.shadowRoot.getElementById('container').style.height = this.height+'px';
       // img element is not present during initial paint
@@ -147,22 +150,23 @@ class GThumb extends HTMLElement {
       }
     }
   }
-  paintSrc(){
+  #paintSrc(){
     if(this.isConnected){
       let img = this.shadowRoot.querySelector('img');
       img.onload = function(){
         this.classList.add('ready');
       };
-      // console.log(`need ${this.height * this.dppx} px`)
+      // console.log(`need ${this.height * this.#dppx} px`)
       img.src = `https://picsum.photos/id/${this.photoid}/${Math.round(this.width)}/${Math.round(this.height)}`;
     } 
   }
-  paintRating(){
+  #paintRating(){
     if(this.isConnected){
+      // setting the value thus will not fire sl-change event
       this.shadowRoot.querySelector('sl-rating').value = this.rating;
     }
   }
-  paintSelected(){
+  #paintSelected(){
     if(this.inisConnected){
       this.shadowRoot.querySelector('input[type="checkbox"]').checked = this.selected;
     }
@@ -170,43 +174,43 @@ class GThumb extends HTMLElement {
   
   // boilerplate stuff
   get photoid(){
-    return this._photoid;
+    return this.#photoid;
   }
   set photoid(_){
-    this._photoid = _;
+    this.#photoid = _;
     // nothing to paint
   }
   
   get width(){
-    return this._width;
+    return this.#width;
   }
   set width(_){
-    this._width = +_;
-    this.paintWidth();
+    this.#width = +_;
+    this.#paintWidth();
   }
   
   get height(){
-    return this._height;
+    return this.#height;
   }
   set height(_){
-    this._height = +_;
-    this.paintHeight();
+    this.#height = +_;
+    this.#paintHeight();
   }
   
   get rating(){
-    return this._rating;
+    return this.#rating;
   }
   set rating(_){
-    this._rating = _;
-    this.paintRating();
+    this.#rating = _;
+    this.#paintRating();
   }
   
   get selected(){
-    return this._selected;
+    return this.#selected;
   }
   set selected(_){
-    this._selected = _
-    this.paintSelected();
+    this.#selected = _
+    this.#paintSelected();
   }
 
 }
